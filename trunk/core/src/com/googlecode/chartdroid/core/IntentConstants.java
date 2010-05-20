31f977c19a16c08827556d0c343a28b15d703ba2
kostmo <kostmo@78023f62-9c30-11de-8595-bbd473b9459f>
2010-05-20 22:22:20 +0000

    documented intent extra constants

--- trunk/core/src/com/googlecode/chartdroid/core/IntentConstants.java
+++ trunk/core/src/com/googlecode/chartdroid/core/IntentConstants.java
@@ -1,26 +1,75 @@
 package com.googlecode.chartdroid.core;
 
 public class IntentConstants {
-	
 
+	/*
+	 * Causes the plot to start in full-screen mode.
+	 */
 	public static final String EXTRA_FULLSCREEN = "com.googlecode.chartdroid.intent.extra.FULLSCREEN";
 
-    
+	 /*
+     * Specify series colors in an array of int values.
+     */
 	public static final String EXTRA_SERIES_COLORS = "com.googlecode.chartdroid.intent.extra.SERIES_COLORS";
+	
+    /*
+     * Specify series labels in an array of String values.
+     */
 	public static final String EXTRA_SERIES_LABELS = "com.googlecode.chartdroid.intent.extra.SERIES_LABELS";
+	
+    /*
+     * Specify series marker styles in an array of int values.
+     * Types:
+     * 0 = x
+     * 1 = circle
+     * 2 = triangle
+     * 3 = square
+     * 4 = diamond
+     * 5 = point
+     */
 	public static final String EXTRA_SERIES_MARKERS = "com.googlecode.chartdroid.intent.extra.SERIES_MARKERS";
+	
+    /*
+     * Specify series line styles in an array of int values.
+     * Types:
+     * 0 = none,
+     * 1 = dotted
+     * 2 = dashed
+     * 3 = solid
+     */
 	public static final String EXTRA_SERIES_LINE_STYLES = "com.googlecode.chartdroid.intent.extra.SERIES_LINE_STYLES";
+	
+    /*
+     * Specify series line thicknesses in an array of float values.
+     */
 	public static final String EXTRA_SERIES_LINE_THICKNESSES = "com.googlecode.chartdroid.intent.extra.SERIES_LINE_THICKNESSES";
 	
-	
+    /*
+     * Specify axis titles in an ArrayList<String>.
+     */
 	public static final String EXTRA_AXIS_TITLES = "com.googlecode.chartdroid.intent.extra.AXIS_TITLES";
     
+	/*
+	 * Format string for the String.format() method, applied to horizontal axis labels.
+	 */
 	public static final String EXTRA_FORMAT_STRING_X = "com.googlecode.chartdroid.intent.extra.FORMAT_STRING_X";
+
+	/*
+	 * Format string for the String.format() method, applied to vertical axis labels.
+	 */
 	public static final String EXTRA_FORMAT_STRING_Y = "com.googlecode.chartdroid.intent.extra.FORMAT_STRING_Y";
 
+	/*
+	 * Boolean to hide/show the horizontal axis. Default: shown.
+	 */
 	public static final String EXTRA_AXIS_VISIBLE_X = "com.googlecode.chartdroid.intent.extra.AXIS_VISIBLE_X";
+
+	/*
+	 * Boolean to hide/show the vertical axis. Default: shown.
+	 */
 	public static final String EXTRA_AXIS_VISIBLE_Y = "com.googlecode.chartdroid.intent.extra.AXIS_VISIBLE_Y";
     
+	
     public static String INTENT_EXTRA_CALENDAR_SELECTION_ID = "INTENT_EXTRA_CALENDAR_SELECTION_ID";
     public static String INTENT_EXTRA_DATE = "INTENT_EXTRA_DATE";
     
