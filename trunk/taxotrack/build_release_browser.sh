c623d5d25faccf51fa0cb22201c54855e727fcae
kostmo <kostmo@78023f62-9c30-11de-8595-bbd473b9459f>
2010-11-16 08:33:00 +0000

    Fixed build scripts

--- trunk/taxotrack/build_release_browser.sh
+++ trunk/taxotrack/build_release_browser.sh
@@ -2,7 +2,7 @@
 
 KEYS_DIR=~/android/keys/taxotrack
 FLICKR_API_KEYS_FILENAME=ApiKeys.java
-FLICKR_API_KEYS_PROJECT_PATH=app/src/com/kostmo/flickr/keys/$FLICKR_API_KEYS_FILENAME
+FLICKR_API_KEYS_PROJECT_PATH=browser/src/org/crittr/keys/$FLICKR_API_KEYS_FILENAME
 
 TEMP_DIRECTORY=temp
 
@@ -11,7 +11,7 @@ mkdir $TEMP_DIRECTORY
 cp $FLICKR_API_KEYS_PROJECT_PATH $TEMP_DIRECTORY
 cp $KEYS_DIR/$FLICKR_API_KEYS_FILENAME $FLICKR_API_KEYS_PROJECT_PATH
 
-cd app
+cd browser
 ant release
 cp bin/TaxoBrowser-release.apk ../
 ant clean
@@ -22,3 +22,12 @@ cp $TEMP_DIRECTORY/$FLICKR_API_KEYS_FILENAME $FLICKR_API_KEYS_PROJECT_PATH
 rm -r $TEMP_DIRECTORY
 
 
+echo -n "Do you want to install the new .apk on your device? [Y/n]: "
+read character
+case $character in
+    [Yy] | "" ) echo "You responded in the affirmative."
+        adb uninstall org.crittr.browse
+        adb install TaxoBrowser-release.apk
+        ;;
+    * ) echo "Fine, then."
+esac
